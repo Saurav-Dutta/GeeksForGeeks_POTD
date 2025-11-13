89deb9717751f46ac30edd750a83f5ff85d3c4e7
SAURAV <sauravdutta029@gmail.com>
2025-11-13 22:31:57 +0530

    Updated Interleaved Strings

--- Interleaved Strings.js
+++ Interleaved Strings.js
@@ -1,32 +1,30 @@
-
 class Solution {
   public:
-    int LCS(string &s1, string &s2){
-        int m = s1.size();
-        int n = s2.size();
-        vector<int> prev(n+1, 0), curr(n+1, 0);
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(s1[i-1] == s2[j-1]){
-                    curr[j] = 1 + prev[j-1];
-                }
-                else{
-                    curr[j] = max(prev[j], curr[j-1]);
-                }
-            }
-            prev = curr;
-        }
-        return prev[n];
-    }
     bool isInterleave(string& s1, string& s2, string& s3) {
-        int n = s1.size();
-        int m = s2.size();
-        int p = s3.size();
-        if(n+m != p) return false;
-        int len_1 = LCS(s1, s3);
-        if(len_1 != n) return false;
-        int len_2 = LCS(s2, s3);
-        if(len_2 != m) return false;
-        return true;
+        int n = s1.size(), m = s2.size(), p = s3.size();
+        if (n + m != p) return false;
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+
+        // Initialize first row
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] && (s1[i - 1] == s3[i - 1]);
+        }
+
+        // Initialize first column
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = dp[0][j - 1] && (s2[j - 1] == s3[j - 1]);
+        }
+
+        // Fill dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+                           (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+            }
+        }
+
+        return dp[n][m];
     }
 };
