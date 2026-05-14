02e7ac70899890afd79bc436be9931c7988566b5
SAURAV <sauravdutta029@gmail.com>
2026-05-14 22:13:32 +0530

    updated Search for Subarray

--- Search for Subarray.js
+++ Search for Subarray.js
@@ -3,32 +3,39 @@ class Solution {
         let n = a.length;
         let m = b.length;
 
-        let result = [];
-
-        if (m === 0) return result;
+        if (m === 0) return [];
 
         // Build LPS array
         let lps = new Array(m).fill(0);
 
-        for (let i = 1, len = 0; i < m;) {
+        let len = 0;
+        for (let i = 1; i < m; ) {
             if (b[i] === b[len]) {
-                lps[i++] = ++len;
-            } else if (len !== 0) {
-                len = lps[len - 1];
+                len++;
+                lps[i] = len;
+                i++;
             } else {
-                lps[i++] = 0;
+                if (len !== 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
 
         // KMP Search
-        for (let i = 0, j = 0; i < n;) {
+        let res = [];
+        let i = 0, j = 0;
+
+        while (i < n) {
             if (a[i] === b[j]) {
                 i++;
                 j++;
             }
 
             if (j === m) {
-                result.push(i - j);
+                res.push(i - j);
                 j = lps[j - 1];
             } else if (i < n && a[i] !== b[j]) {
                 if (j !== 0) {
@@ -39,6 +46,6 @@ class Solution {
             }
         }
 
-        return result;
+        return res;
     }
 }
