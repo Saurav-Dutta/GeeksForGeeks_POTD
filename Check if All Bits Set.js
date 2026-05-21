af23cbab91dc93398562d03cd7b3325b00153fb0
SAURAV <sauravdutta029@gmail.com>
2026-05-21 21:12:54 +0530

    update Check if All Bits Set

--- Check if All Bits Set.js
+++ Check if All Bits Set.js
@@ -1,19 +1,5 @@
 class Solution {
     isBitSet(n) {
-        if (n === 0) {
-            return false;
-        }
-
-        while (n > 0) {
-            let r = n % 2;
-
-            if (r === 0) {
-                return false;
-            }
-
-            n = Math.floor(n / 2);
-        }
-
-        return true;
+        return n > 0 && ((n & (n + 1)) === 0);
     }
 }
